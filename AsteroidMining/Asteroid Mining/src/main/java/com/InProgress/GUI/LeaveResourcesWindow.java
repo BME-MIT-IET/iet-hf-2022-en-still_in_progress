83a1e38a5ece73302ad603e1017bc28bad85d6a3
LR <liuba.rukhlina@gmail.com>
2022-05-14 21:28:30 +0200

    Revert "fix #41"
    This reverts commit b4be68a366ec30d15292401f352ea4f13973efc6.

--- AsteroidMining/Asteroid Mining/src/main/java/com/InProgress/GUI/LeaveResourcesWindow.java
+++ AsteroidMining/Asteroid Mining/src/main/java/com/InProgress/GUI/LeaveResourcesWindow.java
@@ -170,7 +170,13 @@ public class LeaveResourcesWindow extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException|InstantiationException|IllegalAccessException|javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LeaveResourcesWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LeaveResourcesWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LeaveResourcesWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(LeaveResourcesWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
