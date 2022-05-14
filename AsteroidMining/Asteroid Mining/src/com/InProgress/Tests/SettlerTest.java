73174bc0b858a32aa72a5acd50f9fbec457c02f4
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-14 17:20:03 +0200

    Deleted files

---
+++ AsteroidMining/Asteroid Mining/src/com/InProgress/Tests/SettlerTest.java
@@ -0,0 +1,20 @@
+package com.InProgress.Tests;
+
+import com.InProgress.Model.Settler;
+import com.InProgress.Model.*;
+import org.junit.jupiter.api.Test;
+
+class SettlerTest {
+
+    Asteroid testAsteroid = new Asteroid( 0, 0, 0,3);
+    public SettlerTest(){
+
+    }
+
+    @Test
+    void Creation_Settler_Success(){
+
+        //Settler testSettler = new Settler("Test",this.testAsteroid);
+
+    }
+}
