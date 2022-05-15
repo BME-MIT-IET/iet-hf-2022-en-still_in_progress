e3a59685817f2fb4a1b4a6774bcf8cf0fc6f2a1b
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-15 21:49:28 +0200

    Added Settler Build gate Failure Test

--- src/test/java/main/java/com/InProgress/Model/SettlerTest.java
+++ src/test/java/main/java/com/InProgress/Model/SettlerTest.java
@@ -10,37 +10,40 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 class SettlerTest {
     int ROCK_COVER = 3;
-    private Asteroid sourceAsteroid = new Asteroid( 0, 0, 0,3);
-    private Asteroid destinationAsteroid = new Asteroid( 2, 0, 0,3);
-    private Asteroid wrongDestinationAsteroid = new Asteroid( 4, 4, 4,3);
+    private Asteroid sourceAsteroid = new Asteroid(0, 0, 0, 3);
+    private Asteroid destinationAsteroid = new Asteroid(2, 0, 0, 3);
+    private Asteroid wrongDestinationAsteroid = new Asteroid(4, 4, 4, 3);
     private TransportGate sourceGate = new TransportGate();
     private TransportGate destinationGate = new TransportGate();
 
     private Settler testSettler;
+
     @BeforeEach
-    private void Arrange(){
+    private void Arrange() {
         this.destinationAsteroid.setRockCover(3);
         this.destinationAsteroid.setResourceOfAsteroid(new Carbon("Carbon"));
-        this.testSettler = new Settler("Test",this.sourceAsteroid,0);
+        this.testSettler = new Settler("Test", this.sourceAsteroid, 0);
 
     }
 
 
     @Test
-    void Travel_Settler_Success(){
+    void Travel_Settler_Success() {
         testSettler.travel(destinationAsteroid);
-        assertSame(testSettler.getCurrentPosition().getX(),this.destinationAsteroid.getX());
-        assertSame(testSettler.getCurrentPosition().getY(),this.destinationAsteroid.getY());
-        assertSame(testSettler.getCurrentPosition().getZ(),this.destinationAsteroid.getZ());
+        assertSame(testSettler.getCurrentPosition().getX(), this.destinationAsteroid.getX());
+        assertSame(testSettler.getCurrentPosition().getY(), this.destinationAsteroid.getY());
+        assertSame(testSettler.getCurrentPosition().getZ(), this.destinationAsteroid.getZ());
 
     }
+
     @Test
-    void Travel_Settler_Failure(){
+    void Travel_Settler_Failure() {
         testSettler.travel(this.wrongDestinationAsteroid);
-        assertNotEquals(testSettler.getCurrentPosition().getX(),this.wrongDestinationAsteroid.getX());
-        assertNotEquals(testSettler.getCurrentPosition().getY(),this.wrongDestinationAsteroid.getY());
-        assertNotEquals(testSettler.getCurrentPosition().getZ(),this.wrongDestinationAsteroid.getZ());
+        assertNotEquals(testSettler.getCurrentPosition().getX(), this.wrongDestinationAsteroid.getX());
+        assertNotEquals(testSettler.getCurrentPosition().getY(), this.wrongDestinationAsteroid.getY());
+        assertNotEquals(testSettler.getCurrentPosition().getZ(), this.wrongDestinationAsteroid.getZ());
     }
+
     /*
 
     @Test
@@ -60,23 +63,23 @@ class SettlerTest {
 
      */
     @Test
-    void Drill_Settler_Success(){
+    void Drill_Settler_Success() {
         this.sourceAsteroid.setRockCover(ROCK_COVER);
 
         this.testSettler.drill(this.sourceAsteroid);
 
-        assertEquals(ROCK_COVER-1,this.sourceAsteroid.getDepth());
+        assertEquals(ROCK_COVER - 1, this.sourceAsteroid.getDepth());
     }
+
     @Test
-    void Drill_Settler_Failure()
-    {
+    void Drill_Settler_Failure() {
         this.sourceAsteroid.setRockCover(0);
         this.testSettler.drill(this.sourceAsteroid);
-        assertNotEquals(-1,this.sourceAsteroid.getDepth());
+        assertNotEquals(-1, this.sourceAsteroid.getDepth());
     }
+
     @Test
-    void Mine_Settler_Success()
-    {
+    void Mine_Settler_Success() {
         this.sourceAsteroid.setResourceOfAsteroid(new Carbon("Carbon"));
         this.sourceAsteroid.setRockCover(0);
 
@@ -84,11 +87,11 @@ class SettlerTest {
 
         String nameOfResource = this.testSettler.getItsInventory().getStoredResources().get(0).getResourceType();
 
-        assertEquals(nameOfResource,"Carbon");
+        assertEquals(nameOfResource, "Carbon");
     }
+
     @Test
-    void Mine_Settler_Failure()
-    {
+    void Mine_Settler_Failure() {
         this.sourceAsteroid.setResourceOfAsteroid(new Carbon("Carbon"));
         this.sourceAsteroid.setRockCover(1);
 
@@ -96,11 +99,12 @@ class SettlerTest {
 
         List<ResourceBase> emptyList = new ArrayList<ResourceBase>();
 
-        assertEquals(this.testSettler.getItsInventory().getStoredResources(),emptyList);
+        assertEquals(this.testSettler.getItsInventory().getStoredResources(), emptyList);
 
     }
+
     @Test
-    void Build_Gate_Settler_Success(){
+    void Build_Gate_Settler_Success() {
         Inventory gateInventory = new Inventory();
         gateInventory.setStoredResources(new WaterIce("WaterIce"));
         gateInventory.setStoredResources(new Uranium("Uranium"));
@@ -110,7 +114,20 @@ class SettlerTest {
 
         this.testSettler.buildTransportGate();
 
-        assertEquals(2,this.testSettler.getItsInventory().getStoredGates().size());
+        assertEquals(2, this.testSettler.getItsInventory().getStoredGates().size());
     }
+    @Test
+    void Build_Gate_Settler_Failure() {
+        Inventory gateInventory = new Inventory();
+        gateInventory.setStoredResources(new WaterIce("WaterIce"));
+        gateInventory.setStoredResources(new Iron("Iron"));
+        gateInventory.setStoredResources(new Iron("Iron"));
+        this.testSettler.setItsInventory(gateInventory);
+
+        this.testSettler.buildTransportGate();
+
+        assertEquals(0, this.testSettler.getItsInventory().getStoredGates().size());
+    }
+
 
 }
